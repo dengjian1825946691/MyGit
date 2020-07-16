addd1736e0765b4e62041a33e15b97aebdd46b32
dengjian <1825946691@qq.com>
2020-07-16 16:53:23 +0800

    gitDemo2提交

---
+++ src/main/java/com/example/gitdemo2/gg/Student.java
@@ -0,0 +1,16 @@
+package com.example.gitdemo2.gg;
+
+public class Student {
+    private String name;
+    private int age;
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Student() {
+    }
+
+
+}
